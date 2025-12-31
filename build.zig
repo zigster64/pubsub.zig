d1da9d55b5ebd4d90a7cf3dfc88d9d03f7442664
𒀳 Scribe of the Ziggurat𒅆𒂍𒉪 <steve@raffmail.net>
2025-12-31 22:41:02 +1000

    add unit tests - mostly gemini generated here, because I hate writing tests by hand

--- build.zig
+++ build.zig
@@ -28,25 +28,33 @@ pub fn build(b: *std.Build) void {
     const run_cmd = b.addRunArtifact(exe);
     run_step.dependOn(&run_cmd.step);
 
-    run_cmd.step.dependOn(b.getInstallStep());
+    // -------------------------------------------------------------------------
+    // TEST CONFIGURATION
+    // -------------------------------------------------------------------------
+    const test_step = b.step("test", "Run all tests");
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
+    // A. Run tests inside the module itself (if any inline tests exist)
     const mod_tests = b.addTest(.{
         .root_module = mod,
     });
-
     const run_mod_tests = b.addRunArtifact(mod_tests);
-
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
-    const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
+
+    // B. Run the standalone 'tests.zig' file we created
+    // We need to give it access to the 'pubsub' module so it can import it
+    const standalone_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/tests.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+    // This allows @import("pubsub.zig") inside tests.zig to work
+    // Or simpler: tests.zig just uses @import("pubsub.zig") relative path
+    // forcing the test runner to know about the file mapping isn't strictly necessary
+    // if they are in the same folder, but adding the module is cleaner:
+    standalone_tests.root_module.addImport("pubsub_module", mod);
+
+    const run_standalone_tests = b.addRunArtifact(standalone_tests);
+    test_step.dependOn(&run_standalone_tests.step);
 }
---
+++ src/tests.zig
@@ -0,0 +1,278 @@
+const std = @import("std");
+const testing = std.testing;
+const PubSubModule = @import("pubsub.zig");
+const FilterId = PubSubModule.FilterId;
+
+// Access the real Io type from the environment
+const Io = std.Io;
+
+// -------------------------------------------------------------------------
+// 1. TEST PAYLOAD
+// -------------------------------------------------------------------------
+const TestSchema = union(enum) {
+    ping: void,
+    data: struct { id: u32, content: []const u8 },
+    secret: void,
+
+    pub fn needsClone(self: TestSchema) bool {
+        return switch (self) {
+            .ping, .secret => false,
+            .data => true,
+        };
+    }
+
+    pub fn clone(self: TestSchema, arena: std.mem.Allocator) !TestSchema {
+        switch (self) {
+            .ping => return .{ .ping = {} },
+            .secret => return .{ .secret = {} },
+            .data => |d| return .{ .data = .{
+                .id = d.id,
+                .content = try arena.dupe(u8, d.content),
+            } },
+        }
+    }
+};
+
+const PS = PubSubModule.PubSub(TestSchema);
+
+// -------------------------------------------------------------------------
+// 2. UNIT TESTS
+// -------------------------------------------------------------------------
+
+test "PubSub: Basic Signal (Zero Alloc)" {
+    const allocator = testing.allocator;
+
+    // Use Real IO
+    var threaded = Io.Threaded.init(allocator);
+    defer threaded.deinit();
+    const io = threaded.io();
+
+    var pubsub = PS.init(io, allocator);
+    defer pubsub.deinit();
+
+    var sub = try pubsub.client();
+    defer sub.deinit();
+
+    try sub.subscribe(.ping);
+
+    try pubsub.publish(.{ .ping = {} }, .all);
+
+    const event = (try sub.next()) orelse return error.NullEvent;
+    switch (event) {
+        .msg => |m| {
+            defer m.deinit(allocator);
+            try testing.expectEqual(std.meta.Tag(TestSchema).ping, m.topic);
+        },
+        else => return error.WrongEventType,
+    }
+}
+
+test "PubSub: Complex Data (Reference Counting)" {
+    const allocator = testing.allocator;
+
+    var threaded = Io.Threaded.init(allocator);
+    defer threaded.deinit();
+    const io = threaded.io();
+
+    var pubsub = PS.init(io, allocator);
+    defer pubsub.deinit();
+
+    var sub1 = try pubsub.client();
+    defer sub1.deinit();
+    try sub1.subscribe(.data);
+
+    var sub2 = try pubsub.client();
+    defer sub2.deinit();
+    try sub2.subscribe(.data);
+
+    const text = "Hello Reference Counting";
+    try pubsub.publish(.{ .data = .{ .id = 42, .content = text } }, .all);
+
+    // Consumer 1 verifies
+    {
+        const event = (try sub1.next()) orelse return error.NullEvent;
+        switch (event) {
+            .msg => |m| {
+                defer m.deinit(allocator);
+                try testing.expectEqual(m.payload.data.id, 42);
+                try testing.expectEqualStrings(m.payload.data.content, text);
+            },
+            else => return error.WrongEventType,
+        }
+    }
+
+    // Consumer 2 verifies (Tests shared ownership)
+    {
+        const event = (try sub2.next()) orelse return error.NullEvent;
+        switch (event) {
+            .msg => |m| {
+                defer m.deinit(allocator);
+                try testing.expectEqualStrings(m.payload.data.content, text);
+            },
+            else => return error.WrongEventType,
+        }
+    }
+}
+
+test "PubSub: Filter Routing" {
+    const allocator = testing.allocator;
+
+    var threaded = Io.Threaded.init(allocator);
+    defer threaded.deinit();
+    const io = threaded.io();
+
+    var pubsub = PS.init(io, allocator);
+    defer pubsub.deinit();
+
+    const uuid_a = 0xAAAA_AAAA_AAAA_AAAA;
+    const filter_a = FilterId.from(uuid_a);
+
+    var sub_a = try pubsub.client();
+    defer sub_a.deinit();
+    try sub_a.subscribe(.ping);
+    sub_a.setFilter(filter_a);
+
+    var sub_b = try pubsub.client();
+    defer sub_b.deinit();
+    try sub_b.subscribe(.ping);
+
+    // 1. Broadcast -> Both
+    try pubsub.publish(.{ .ping = {} }, .all);
+
+    if ((try sub_a.next())) |e| {
+        if (e == .msg) e.msg.deinit(allocator);
+    } else return error.ExpectedMsgA;
+    if ((try sub_b.next())) |e| {
+        if (e == .msg) e.msg.deinit(allocator);
+    } else return error.ExpectedMsgB;
+
+    // 2. Targeted A -> Both (because B is .all)
+    try pubsub.publish(.{ .ping = {} }, filter_a);
+
+    if ((try sub_a.next())) |e| {
+        if (e == .msg) e.msg.deinit(allocator);
+    } else return error.ExpectedMsgA_Targeted;
+    if ((try sub_b.next())) |e| {
+        if (e == .msg) e.msg.deinit(allocator);
+    } else return error.ExpectedMsgB_Targeted;
+
+    // 3. Targeted B -> A should NOT get it
+    const filter_b = FilterId.from(0xBBBB_BBBB);
+    try pubsub.publish(.{ .ping = {} }, filter_b);
+
+    sub_a.setTimeout(1);
+    const res = try sub_a.next();
+    if (res) |e| {
+        switch (e) {
+            .msg => |m| {
+                m.deinit(allocator);
+                return error.ShouldNotReceive;
+            },
+            .timeout => {},
+        }
+    }
+}
+
+// -------------------------------------------------------------------------
+// 3. E2E CONCURRENCY TEST
+// -------------------------------------------------------------------------
+
+const E2EContext = struct {
+    ps: *PS,
+};
+
+fn producer_thread(io: Io, ctx: *E2EContext) void {
+    var i: u32 = 0;
+    while (i < 100) : (i += 1) {
+        ctx.ps.publish(.{ .data = .{ .id = i, .content = "data" } }, .all) catch {};
+        // Tiny sleep to yield
+        std.Io.sleep(io, .fromNanoseconds(10_000), .real) catch {};
+    }
+    ctx.ps.publish(.{ .ping = {} }, .all) catch {};
+}
+
+test "PubSub: E2E Concurrent Producer/Consumer" {
+    const allocator = testing.allocator;
+
+    var threaded = Io.Threaded.init(allocator);
+    defer threaded.deinit();
+    const io = threaded.io();
+
+    var pubsub = PS.init(io, allocator);
+    defer pubsub.deinit();
+
+    var ctx = E2EContext{ .ps = &pubsub };
+
+    // 1. SETUP CONSUMER FIRST (So we don't miss any messages)
+    var sub = try pubsub.client();
+    defer sub.deinit();
+    try sub.subscribe(.data);
+    try sub.subscribe(.ping);
+
+    // 2. NOW SPAWN PRODUCER
+    const t_prod = try std.Thread.spawn(.{}, producer_thread, .{ io, &ctx });
+    defer t_prod.join();
+
+    // 3. START CONSUMING
+    var count: u32 = 0;
+    while (true) {
+        const event = (try sub.next()) orelse break;
+        switch (event) {
+            .msg => |m| {
+                defer m.deinit(allocator);
+                switch (m.topic) {
+                    .data => count += 1,
+                    .ping => break,
+                    else => {},
+                }
+            },
+            .timeout => {},
+        }
+    }
+
+    // Now we should get exactly 100
+    try testing.expectEqual(count, 100);
+}
+
+test "PubSub: E2E Concurrent Producer/Consumer out of order" {
+    const allocator = testing.allocator;
+
+    var threaded = Io.Threaded.init(allocator);
+    defer threaded.deinit();
+    const io = threaded.io();
+
+    var pubsub = PS.init(io, allocator);
+    defer pubsub.deinit();
+
+    var ctx = E2EContext{ .ps = &pubsub };
+
+    const t_prod = try std.Thread.spawn(.{}, producer_thread, .{ io, &ctx });
+    defer t_prod.join();
+
+    // producer is pumping away - we defs going to miss the first few
+    // if we dont, then there is something horribly out of whack with the threading
+    // implementation
+
+    var sub = try pubsub.client();
+    defer sub.deinit();
+    try sub.subscribe(.data);
+    try sub.subscribe(.ping);
+
+    var count: u32 = 0;
+    while (true) {
+        const event = (try sub.next()) orelse break;
+        switch (event) {
+            .msg => |m| {
+                defer m.deinit(allocator);
+                switch (m.topic) {
+                    .data => count += 1,
+                    .ping => break,
+                    else => {},
+                }
+            },
+            .timeout => {},
+        }
+    }
+
+    try testing.expect(count < 100);
+}
